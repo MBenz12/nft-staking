10f94936d1baeddd3cd8df4c20bed0df0a20f9a1
mbenz <mbenz10120112@gmail.com>
2022-07-25 10:08:39 -0400

    init project

---
+++ programs/nft-staking/src/lib.rs
@@ -0,0 +1,22 @@
+use anchor_lang::prelude::*;
+
+use instructions::*;
+
+mod instructions;
+mod state;
+mod utils;
+mod costatn;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod nft_staking {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
